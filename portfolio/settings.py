e737bf124f6d73f14782a45995e7f147703746cc
vishwajeet vijay jagtap <vishwajeet300@gmail.com>
2021-06-07 05:03:43 +0530

    stat

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 #  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
