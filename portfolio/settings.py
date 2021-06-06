3e68ebb05bfe33b363d11545340a46722c346aca
vishwajeet vijay jagtap <vishwajeet300@gmail.com>
2021-06-07 03:19:11 +0530

    static file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # for production
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
