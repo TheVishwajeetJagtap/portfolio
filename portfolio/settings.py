9e22bb50490a9285f75b7d45398fc7118007193d
vishwajeet vijay jagtap <vishwajeet300@gmail.com>
2021-06-07 04:46:22 +0530

    static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
